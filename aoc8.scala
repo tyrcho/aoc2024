59bad1e3dc9924c1492107cf74277060e31ad386
Michel Daviot <michel@daviot.info>
2024-12-08 20:18:55 +0100

    day 8 part 2

--- aoc8.scala
+++ aoc8.scala
@@ -3,7 +3,7 @@
 //> using dep org.scalameta::munit:1.0.3
 
 def main(args: Array[String]): Unit =
-    println(process(io.Source.stdin))
+    println(process2(io.Source.stdin))
 
 case class Game(data: Seq[String]) {
     val ids: Set[Char] = data.flatMap(_.toSeq.toSet).toSet - '.'
@@ -23,21 +23,43 @@ case class Game(data: Seq[String]) {
             if inGrid(symetric)
         } yield symetric
 
+    def allInLine: Set[Pos] =
+        for {
+            id <- ids
+            Seq(p1, p2) <- positionsFor(id).combinations(2)
+            (line1, line2) = p1.inLine(p2)
+            p <- line1.takeWhile(inGrid) ++ line2.takeWhile(inGrid)
+        } yield p
+
     def inGrid(p: Pos) = p.r >= 0 && p.c >= 0 && p.r < data.size && p.c < data.head.size
 
 }
 
 case class Pos(r: Int, c: Int) {
     def symetrics(that: Pos) =
-        Set(
-            Pos(2 * this.r - that.r, 2 * this.c - that.c),
-            Pos(2 * that.r - this.r, 2 * that.c - this.c)
+        Set(this * 2 - that, that * 2 - this)
+
+    def inLine(that: Pos): (Iterator[Pos], Iterator[Pos]) = {
+        val vector = Pos(that.r - this.r, that.c - this.c)
+        (
+            Iterator.tabulate(1000)(i => this + vector * i),
+            Iterator.tabulate(1000)(i => this - vector * i)
         )
+    }
+
+    def *(x: Int) = Pos(r * x, c * x)
+
+    def +(that: Pos) = Pos(this.r + that.r, this.c + that.c)
+    def -(that: Pos) = Pos(this.r - that.r, this.c - that.c)
 }
 
 def process(source: io.Source): Int =
     val game = parse(source)
     game.validSymetrics.size
+
+def process2(source: io.Source): Int =
+    val game = parse(source)
+    game.allInLine.size
 
 
 def parse(source: io.Source): Game =
@@ -61,6 +83,10 @@ class TestDay8Suite extends munit.FunSuite {
 
     test("part 1") {
         assertEquals(process(io.Source.fromString(shortSample)), 14)
+    }
+
+    test("part 2") {
+        assertEquals(process2(io.Source.fromString(shortSample)), 34)
     }
 
 
